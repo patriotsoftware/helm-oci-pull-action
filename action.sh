f1674ec503ceff487f2682d6e394837b0c1c12e4
Steve Yackey <syackey@patriotsoftware.com>
2021-06-18 08:51:31 -0400

    Testing initial release

---
+++ action.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+HELM_EXPERIMENTAL_OCI=1
+set -e
+if [[ "${INPUT_ECR_LOGIN}" == "true" ]]; then
+    aws ecr get-login-password --region ${INPUT_AWS_REGION} | helm registry login --username AWS --password-stdin "${INPUT_AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
+    echo "✅ AWS ECR Login Complete"
+fi
+helm chart pull "${INPUT_CHART_AND_TAG}" 
+echo "✅ ${INPUT_CHART_AND_TAG} pulled successfully"
+helm chart "${INPUT_CHART_AND_TAG}" --destination "${INPUT_RELATIVE_TARGET_DIRECTORY}"
+echo "✅ ${INPUT_CHART_AND_TAG} saved to ${INPUT_RELATIVE_TARGET_DIRECTORY} successfully"
+echo "::set-output name=chart-path::$(pwd)/${INPUT_RELATIVE_TARGET_DIRECTORY}"
