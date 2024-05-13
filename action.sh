a342baa6a41fcf39bb5cbd4dfa1312e607c71cf9
Nate O'Neill <noneill@patriotsoftware.com>
2024-05-13 16:15:33 -0400

    Update action.sh

--- action.sh
+++ action.sh
@@ -5,8 +5,20 @@ if [[ "${INPUT_ECR_LOGIN}" == "true" ]]; then
     aws ecr get-login-password --region ${INPUT_AWS_REGION} | helm registry login --username AWS --password-stdin "${INPUT_AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
     echo "✅ AWS ECR Login Complete"
 fi
-helm chart pull "${INPUT_CHART_AND_TAG}" 
-echo "✅ ${INPUT_CHART_AND_TAG} pulled successfully"
-helm chart export "${INPUT_CHART_AND_TAG}" --destination "${INPUT_TARGET_DIRECTORY}"
-echo "✅ ${INPUT_CHART_AND_TAG} saved to ${INPUT_TARGET_DIRECTORY} successfully"
-echo "::set-output name=chart-path::${INPUT_TARGET_DIRECTORY}"
+
+HELM_VERSION="$(helm version --template='Version: {{.Version}}' )"
+HELM_VERSION=$(echo $HELM_VERSION | sed 's/[^0-9]*//g')
+
+if [ $HELM_VERSION -gt 370 ]
+then
+    helm pull "oci://${INPUT_CHART_AND_TAG}" --untar --untardir "${INPUT_TARGET_DIRECTORY}"
+    echo "✅ ${INPUT_CHART_AND_TAG} pulled successfully"
+    echo "✅ ${INPUT_CHART_AND_TAG} saved to ${INPUT_TARGET_DIRECTORY} successfully"
+    echo "::set-output name=chart-path::${INPUT_TARGET_DIRECTORY}"
+else
+    helm chart pull "${INPUT_CHART_AND_TAG}" 
+    echo "✅ ${INPUT_CHART_AND_TAG} pulled successfully"
+    helm chart export "${INPUT_CHART_AND_TAG}" --destination "${INPUT_TARGET_DIRECTORY}"
+    echo "✅ ${INPUT_CHART_AND_TAG} saved to ${INPUT_TARGET_DIRECTORY} successfully"
+    echo "::set-output name=chart-path::${INPUT_TARGET_DIRECTORY}"
+fi
