f733b97115ca09163a3538ec176c4ec3b41a086e
Nate O'Neill <noneill@patriotsoftware.com>
2024-05-13 17:19:31 -0400

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export HELM_EXPERIMENTAL_OCI=1
 set -e
 if [[ "${INPUT_ECR_LOGIN}" == "true" ]]; then
     aws ecr get-login-password --region ${INPUT_AWS_REGION} | helm registry login --username AWS --password-stdin "${INPUT_AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
@@ -15,7 +16,6 @@ then
     echo "✅ ${INPUT_CHART_AND_TAG} saved to ${INPUT_TARGET_DIRECTORY} successfully"
     echo "::set-output name=chart-path::${INPUT_TARGET_DIRECTORY}"
 else
-    export HELM_EXPERIMENTAL_OCI=1
     helm chart pull "${INPUT_CHART_AND_TAG}" 
     echo "✅ ${INPUT_CHART_AND_TAG} pulled successfully"
     helm chart export "${INPUT_CHART_AND_TAG}" --destination "${INPUT_TARGET_DIRECTORY}"
