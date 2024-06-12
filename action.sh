003489476fce3bca79f26ace130f8a285c77912a
Nate O'Neill <noneill@patriotsoftware.com>
2024-06-12 14:32:06 -0400

    Update action.sh (#6)
    * Update action.sh
    
    * Remove debug lines and empty whitespace
    
    * Update action.sh
    
    * Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export HELM_EXPERIMENTAL_OCI=1
 set -e
+
 if [[ "${INPUT_ECR_LOGIN}" == "true" ]]; then
     aws ecr get-login-password --region ${INPUT_AWS_REGION} | helm registry login --username AWS --password-stdin "${INPUT_AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
     echo "✅ AWS ECR Login Complete"
@@ -11,8 +12,13 @@ HELM_VERSION=$(echo $HELM_VERSION | sed 's/[^0-9]*//g')
 
 if [ $HELM_VERSION -ge 380 ]
 then
-    helm pull "oci://${INPUT_CHART_AND_TAG}" --untar --untardir "${INPUT_TARGET_DIRECTORY}"
-    echo "✅ ${INPUT_CHART_AND_TAG} pulled successfully"
+    if [[ ${INPUT_CHART_AND_TAG} == *"monochart:latest"* ]]; then
+        helm pull "oci://${INPUT_AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/monochart" --untar --untardir "${INPUT_TARGET_DIRECTORY}"
+        echo "✅ Latest Monochart pulled successfully"
+    else
+        helm pull "oci://${INPUT_CHART_AND_TAG}" --untar --untardir "${INPUT_TARGET_DIRECTORY}"
+        echo "✅ ${INPUT_CHART_AND_TAG} pulled successfully"
+    fi
     echo "✅ ${INPUT_CHART_AND_TAG} saved to ${INPUT_TARGET_DIRECTORY} successfully"
     echo "::set-output name=chart-path::${INPUT_TARGET_DIRECTORY}"
 else
